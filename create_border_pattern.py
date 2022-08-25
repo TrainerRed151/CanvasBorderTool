04da6478cf6ad3b7555b07115f67f27e1a794ff0
Brian Pomerantz <br.pomerantz@gmail.com>
2022-08-24 22:44:37 -0600

    function compartmentalization

--- create_border_pattern.py
+++ create_border_pattern.py
@@ -1,45 +1,73 @@
 from PIL import Image
 import sys
 
-if len(sys.argv) < 2:
-    print('Please provide a file.')
-    exit()
 
-filename = sys.argv[1]
-im = Image.open(filename)
+def read_image():
+    if len(sys.argv) < 2:
+        print('Please provide a file.')
+        exit()
 
-h_off = int(0.1*im.height)
-w_off = int(0.1*im.width)
-offset = max(h_off, w_off)
+    filename = sys.argv[1]
+    print(f'Reading image {filename}')
+    return Image.open(filename), filename
 
-w2 = im.width + 2*offset
-h2 = im.height + 2*offset
-i2 = Image.new('RGB', (w2, h2))
 
-for h in range(im.height):
-    for w in range(im.width):
-        v = im.getpixel((w, h))
-        i2.putpixel((w+offset, h+offset), v)
+def calculate_offset(im):
+    h_off = int(0.1*im.height)
+    w_off = int(0.1*im.width)
 
-for h in range(im.height):
-    for w in range(offset):
-        vl = im.getpixel((w, h))
-        i2.putpixel((offset-w, h+offset), vl)
+    return max(h_off, w_off)
 
-        vr = im.getpixel((im.width-w-1, h))
-        i2.putpixel((im.width+offset+w, h+offset), vr)
 
-for h in range(offset):
-    for w in range(im.width):
-        vt = im.getpixel((w, h))
-        i2.putpixel((w+offset, offset-h), vt)
+def create_border_image(im, offset):
+    print('Creating new image...')
+    w2 = im.width + 2*offset
+    h2 = im.height + 2*offset
 
-        vb = im.getpixel((w, im.height-h-1))
-        i2.putpixel((w+offset, im.height+offset+h), vb)
+    i2 = Image.new('RGB', (w2, h2))
+    _fill_border(im, i2, offset)
 
-im.close()
-base, ext = filename.split('.')
-i2.save(f'{base}_borders.{ext}')
-i2.close()
+    return i2
 
-print(f'New file created at: {base}_borders.{ext}')
+
+def _fill_border(im, i2, offset):
+    print('Copying original pixels...')
+    for h in range(im.height):
+        for w in range(im.width):
+            v = im.getpixel((w, h))
+            i2.putpixel((w+offset, h+offset), v)
+
+    print('Creating left/right borders...')
+    for h in range(im.height):
+        for w in range(offset):
+            vl = im.getpixel((w, h))
+            i2.putpixel((offset-w, h+offset), vl)
+
+            vr = im.getpixel((im.width-w-1, h))
+            i2.putpixel((im.width+offset+w, h+offset), vr)
+
+    print('Creating top/bottom borders...')
+    for h in range(offset):
+        for w in range(im.width):
+            vt = im.getpixel((w, h))
+            i2.putpixel((w+offset, offset-h), vt)
+
+            vb = im.getpixel((w, im.height-h-1))
+            i2.putpixel((w+offset, im.height+offset+h), vb)
+
+
+def main():
+    im, filename = read_image()
+    offset = calculate_offset(im)
+    i2 = create_border_image(im, offset)
+
+    im.close()
+    base, ext = filename.split('.')
+    i2.save(f'{base}_borders.{ext}')
+    i2.close()
+
+    print(f'New file created at: {base}_borders.{ext}')
+
+
+if __name__ == '__main__':
+    main()
